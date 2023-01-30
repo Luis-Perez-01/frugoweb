d61a1cd8fdafce77441a8fb30b1ec496b6104b98
Luis Perez <luism1p3@hotmail.com>
2023-01-30 13:43:16 -0600

    Vista welcome

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,26 +1,29 @@
 <!doctype html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Frugo | @yield('title', 'Frugo')</title>
+    <title>@yield('title', 'Frugo')</title>
+    <link rel="icon" href="{{asset('assets/favicon.ico')}}" />
     <link href="https://fonts.cdnfonts.com/css/black-jack" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/layoutStyle.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Frugo</a>
+    <nav class="navbar navbar-expand-lg sticky-top bg-white">
+        <div class="container">
+            <a class="navbar-brand" href="#"><img src="{{asset('assets/logo.png')}}" class="img-fluid mx-auto pt-3" alt="..." style="padding-left: 6rem;" /></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-uppercase">
-                    <li class="nav-item dropdown">
+                    <li class="nav-item dropdown px-4">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Acerca de Frugo
+                            <strong>Acerca <br> de Frugo</strong>
                         </a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="#">Historia</a></li>
@@ -29,32 +32,33 @@
                             <li><a class="dropdown-item" href="#">Nuestra Filosofía</a></li>
                         </ul>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Nuestros Productos</a>
+                    <li class="nav-item px-4">
+                        <a class="nav-link active" aria-current="page" href="#"><strong>Nuestros <br> Productos</strong></a>
                     </li>
-                    <li class="nav-item dropdown">
+                    <li class="nav-item dropdown px-4">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            La calidad nos distingue
+                            <strong>La calidad <br> nos distingue</strong>
                         </a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="#">Certificaciones</a></li>
                             <li><a class="dropdown-item" href="#">Agrícultura</a></li>
                         </ul>
                     </li>
+                    <li class="nav-item px-5">
+                    </li>
+
                 </ul>
+                <div class="btnNav">
+                    <a href="" class="text-center"><i class='bx bxs-group bx-sm'></i></a>
+                    <a href="" class="text-center"><i class='bx bx-envelope bx-sm'></i></a>
+                </div>
             </div>
         </div>
     </nav>
-    <div class="container-fluid p-0">
-        <img src="{{URL::asset('assets/bg-home-vfinal.png')}}" class="d-block w-100" alt="..." />
-        <div class="text-center" style="position: absolute; top: 500px; left: 0; right: 0; margin: auto;">
-            <span style="color: #3FAE29; font-family: BlackJack; font-weight: 100; font-size: 40px;">Vive bien come lo mejor</span>
-            <img src="{{URL::asset('assets/scribble.png')}}" class="img-fluid mx-auto" alt="..." />
-        </div>
-    </div>
-
     @section('content')
-    @endsection
+    @show
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 
